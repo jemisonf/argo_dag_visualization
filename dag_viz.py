f4d9eebc9fb21d53a4d913d11cb3df5b296a035b
Fischer Jemison <fjemison@newrelic.com>
2020-11-17 10:25:37 -0800

    initial commit

---
+++ dag_viz.py
@@ -0,0 +1,48 @@
+import argparse
+import yaml
+
+parser = argparse.ArgumentParser(description="visualize argo workflow DAGs")
+parser.add_argument("file", type=str, help="the path to the workflow file")
+parser.add_argument(
+    "template", type=str, help="the template to render", nargs="*", default=""
+)
+args = parser.parse_args()
+
+path = args.file
+template_name = args.template
+
+wf = {}
+
+try:
+    with open(path) as f:
+        wf = yaml.load(f, Loader=yaml.FullLoader)
+except:
+    print(f"file {path} not found")
+    exit(1)
+
+templates = wf["spec"]["templates"]
+
+if template_name == "":
+    print(f"Please use one of the following templates:")
+    for template in templates:
+        name = template["name"]
+        print(f"\t{name}")
+    exit(1)
+
+with open("output.gv", "w+") as f:
+
+    for template in templates:
+        name = template["name"].replace("-", "_")
+        if "dag" not in template:
+            continue
+
+        f.write(f"digraph {name} {{\n")
+        tasks = template["dag"]["tasks"]
+
+        for task in tasks:
+            name = task["name"].replace("-", "_")
+            for dependency in task.get("dependencies", []):
+                dep_name = dependency.replace("-", "_")
+                f.write(f"\t{dep_name} -> {name}\n")
+
+        f.write("}\n")
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Argo DAG Visualizer
+
+This is a CLI for creating graphviz visualizations of Argo Workflows DAG templates. It's written for Python 3.9 but is probably compatible with Python >=3.6 (f-string support required).
+
+To run, first run
+
+```
+pip3 install -r requirements.txt
+```
+
+Then:
+
+```
+python3 ./dag_viz path_to_your_workflow.yaml template-name
+```
+
+You will get an output in your current working directory called `output.gv`. You can render it with
+
+```
+dot -Tpng -o graph.png output.gv
+```
+
+And then open `graph.png` to see the rendered graph.
+
